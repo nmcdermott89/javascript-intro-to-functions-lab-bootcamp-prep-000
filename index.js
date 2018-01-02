cac9dee4b1e5e11e482ed499cf752e2f8fb325ad
Nicholas McDermott <nmcdermott89@gmail.com>
2018-01-02 05:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ function logShout (string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+
+
+function sayHiToGrandma(string) {
+
+if (string === string.toLowerCase)
+
+return "I can't hear you!"
+
+else if (string === string.toUpperCase)
+
+return "YES INDEED!"
+
+if (string === "I love you, Grandma.")
+
+return "I love you, too."
+}
+  
