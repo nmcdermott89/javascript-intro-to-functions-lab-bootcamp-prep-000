cef8fc5a85f7d2b38e98b7edb9b0aefbbc3b25f8
Nicholas McDermott <nmcdermott89@gmail.com>
2018-01-02 05:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function shout(string) {
-  console.log(string.toUpperCase())
   return string
 }
 
 function whisper(string) {
   return string.toLowerCase()
+}
+
+
+function logShout (string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
